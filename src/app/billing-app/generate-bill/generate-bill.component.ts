351b42029238a8597d7ddc63f35513be76280d69
kakadeos <kakadeos@gmail.com>
2019-06-27 16:50:12 +0530

    Completed Form and View

--- src/app/billing-app/generate-bill/generate-bill.component.ts
+++ src/app/billing-app/generate-bill/generate-bill.component.ts
@@ -35,6 +35,8 @@ export class GenerateBillComponent implements OnInit {
   itemCost: number;
   itemSubTotalCost: number;
   itemGstValue: number;
+  itemCGSTValue: number;
+  itemSGSTValue: number;
   itemCGST: number;
   itemSGST: number;
   itemTotalCost: number;
@@ -45,31 +47,31 @@ export class GenerateBillComponent implements OnInit {
 
   ngOnInit() {
     this.form = new FormGroup({
-        'InvoiceNumber': new FormControl('Enter Invoice Number', {validators: [Validators.required]}),
+        'InvoiceNumber': new FormControl('Invoice Number', {validators: [Validators.required]}),
         'CompanyName': new FormControl('Enter Company Name', {validators: [Validators.required]}),
         'CompanyAddressInitial': new FormControl('Enter Company Address 1', {validators: [Validators.required]}),
-        'CompanyAddressPart2': new FormControl(''),
-        'CompanyCity': new FormControl(''),
-        'CompanyState': new FormControl(''),
-        'CompanyCountry': new FormControl(''),
-        'CompanyPincode': new FormControl(''),
-        'CompanyGSTN': new FormControl(''),
+        'CompanyAddressPart2': new FormControl('Enter Company Address 2'),
+        'CompanyCity': new FormControl('Enter Company City'),
+        'CompanyState': new FormControl('Enter Comapny State'),
+        'CompanyCountry': new FormControl('Enter Company Country'),
+        'CompanyPincode': new FormControl('Enter Company PinCode'),
+        'CompanyGSTN': new FormControl('Enter Company GSTN'),
         'ToCompanyName': new FormControl('Enter Company Name', {validators: [Validators.required]}),
         'ToCompanyAddressInitial': new FormControl('Enter Company Address 1', {validators: [Validators.required]}),
-        'ToCompanyAddressPart2': new FormControl(''),
-        'ToCompanyCity': new FormControl(''),
-        'ToCompanyState': new FormControl(''),
-        'ToCompanyCountry': new FormControl(''),
-        'ToCompanyPincode': new FormControl(''),
-        'ToCompanyGSTN': new FormControl(''),
-        'InvoiceDate': new FormControl(''),
-        'InvoiceTerms': new FormControl(''),
-        'ItemNumber': new FormControl(''),
-        'ItemName': new FormControl(''),
-        'ItemQty': new FormControl(''),
-        'ItemRate': new FormControl(''),
-        'ItemGSTValue': new FormControl(''),
-        'InvoiceNotes': new FormControl('')
+        'ToCompanyAddressPart2': new FormControl('Enter Company Address 2'),
+        'ToCompanyCity': new FormControl('Enter Company City'),
+        'ToCompanyState': new FormControl('Enter Comapny State'),
+        'ToCompanyCountry': new FormControl('Enter Company Country'),
+        'ToCompanyPincode': new FormControl('Enter Company PinCode'),
+        'ToCompanyGSTN': new FormControl('Enter Company GSTN'),
+        'InvoiceDate': new FormControl('Enter Invoice Date'),
+        'InvoiceTerms': new FormControl('Enter Invoice Terms'),
+        'ItemNumber': new FormControl('Enter Item Number'),
+        'ItemName': new FormControl('Enter Item Name'),
+        'ItemQty': new FormControl('Enter Item Quantity'),
+        'ItemRate': new FormControl('Enter Item Rate'),
+        'ItemGSTValue': new FormControl('Enter GST Value'),
+        'InvoiceNotes': new FormControl('Enter Invoice Notes')
     });
 
     this.invoiceNumber = 'INV-0004';
@@ -95,6 +97,8 @@ export class GenerateBillComponent implements OnInit {
     //this.addItem(2, 'WebApp Social Media', 3, 8000);
     this.calculatSubTotalCost();
     this.itemGstValue = 18;
+    this.itemCGSTValue = this.itemGstValue / 2;
+    this.itemSGSTValue = this.itemGstValue /2;
     this.calculateGST(this.itemGstValue);
     this.calculateItemTotalCost();
     this.invoiceNotes = 'Notes Added here';
@@ -109,6 +113,7 @@ export class GenerateBillComponent implements OnInit {
     this.form.get('ItemQty').setValue(null);
     this.form.get('ItemRate').setValue(null);
     this.calculatSubTotalCost();
+    this.calculateGST(this.itemGstValue);
   }
 
   calculatSubTotalCost() {
@@ -120,12 +125,10 @@ export class GenerateBillComponent implements OnInit {
   }
 
   calculateGST(itemGstValue) {
-    console.log(itemGstValue);
     var halfGSTValue = itemGstValue / 2;
-    console.log(halfGSTValue);
+    this.itemCGSTValue = halfGSTValue;
+    this.itemSGSTValue = halfGSTValue;
     var calulateGSTPart1 = (this.itemSubTotalCost * halfGSTValue);
-    console.log(calulateGSTPart1);
-    console.log(calulateGSTPart1);
     this.itemCGST = calulateGSTPart1 / 100;
     this.itemSGST = calulateGSTPart1 / 100;
     this.calculateItemTotalCost();
@@ -133,10 +136,35 @@ export class GenerateBillComponent implements OnInit {
 
   calculateItemTotalCost() {
     this.itemTotalCost = this.itemSubTotalCost + this.itemCGST + this.itemSGST ;
-    console.log(this.itemTotalCost);
   }
 
   generateBill() {
+      console.log(this.form);
+      console.log(this.itemList);
+  }
 
+  deleteItem(itemNumber) {
+    for (let index = 0; index < this.itemList.length; index++) {
+      const element = this.itemList[index];
+      if (element.itemNumber == itemNumber) {
+       this.itemList.splice(index, 1);
+      }
+    }
+    this.calculatSubTotalCost();
+    this.calculateGST(this.itemGstValue);
+  }
+
+  step = 0;
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
   }
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule,
   MatInputModule,
   MatFormFieldModule, MatDatepickerModule,
-  MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule, MatSnackBarModule, MatDialogModule, MatCardModule
+  MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule, MatSnackBarModule, MatDialogModule, MatCardModule, MatExpansionModule
  } from '@angular/material';
 
 @NgModule({
   imports: [MatButtonModule, MatInputModule, MatFormFieldModule,MatDatepickerModule, MatNativeDateModule
   ,MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule,
-MatListModule, MatSnackBarModule, MatDialogModule, MatCardModule],
+MatListModule, MatSnackBarModule, MatDialogModule, MatCardModule,MatExpansionModule],
   exports: [MatButtonModule, MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule
     ,MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule,
-    MatListModule, MatSnackBarModule, MatDialogModule, MatCardModule]
+    MatListModule, MatSnackBarModule, MatDialogModule, MatCardModule, MatExpansionModule]
 })
 export class MaterialModule {
 
