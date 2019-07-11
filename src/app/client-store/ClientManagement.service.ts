c3ccaa95c0c3a4b7722d0b6a04423b1d5744d046
kakadeos <kakadeos@gmail.com>
2019-07-11 16:29:37 +0530

    Client Management Option Added

--- src/app/client-store/ClientManagement.service.ts
+++ src/app/client-store/ClientManagement.service.ts
@@ -6,71 +6,99 @@ import { Client } from './Client.module';
 import { Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class ClientManagementService {
 
-  msg : string;
+  msg: string;
   private clients: Client[] = [];
-  private clientsUpdated =  new Subject<Client[]>();
+  private clientsUpdated = new Subject<Client[]>();
 
   constructor(private http: HttpClient, private router: Router,
-    private snackBar: MatSnackBar) {}
+    private snackBar: MatSnackBar) { }
 
-    AddNewClient(CompanyName: string, CompanyAddressInitial: string, CompanyAddressPart2: string,
-      CompanyCity: string, CompanyPincode: string, CompanyState: string, CompanyCountry: string,
-      CompanyGSTN: string) {
-      const Client: Client = {
-        id: null,
-        ClientCompanyName: CompanyName,
-        ClientCompanyAddressInitial: CompanyAddressInitial,
-        ClientCompanyAddressPart2: CompanyAddressPart2,
-        ClientCompanyCity: CompanyCity,
-        ClientCompanyState: CompanyState,
-        ClientCompanyCountry: CompanyCountry,
-        ClientCompanyPincode: CompanyPincode,
-        ClientCompanyGSTN: CompanyGSTN
-      };
-      this.http.post<{ message: string, result: object }>('http://localhost:3000/api/clients/addNewClient', Client)
-        .subscribe(response => {
-          this.snackBar.open(response.message, null, { duration: 3000 });
-        },
-          (error) => {
-            this.msg = error.error.message;
-            if (this.msg === undefined) {
-              this.msg = error.message;
-            }
-            this.snackBar.open(this.msg, null, { duration: 3000 });
-          });
-    }
+  AddNewClient(CompanyName: string, CompanyAddressInitial: string, CompanyAddressPart2: string,
+    CompanyCity: string, CompanyPincode: string, CompanyState: string, CompanyCountry: string,
+    CompanyGSTN: string) {
+    const Client: Client = {
+      id: null,
+      ClientCompanyName: CompanyName,
+      ClientCompanyAddressInitial: CompanyAddressInitial,
+      ClientCompanyAddressPart2: CompanyAddressPart2,
+      ClientCompanyCity: CompanyCity,
+      ClientCompanyState: CompanyState,
+      ClientCompanyCountry: CompanyCountry,
+      ClientCompanyPincode: CompanyPincode,
+      ClientCompanyGSTN: CompanyGSTN
+    };
+    this.http.post<{ message: string, result: object }>('http://localhost:3000/api/clients/addNewClient', Client)
+      .subscribe(response => {
+        this.snackBar.open(response.message, null, { duration: 3000 });
+      },
+        (error) => {
+          this.msg = error.error.message;
+          if (this.msg === undefined) {
+            this.msg = error.message;
+          }
+          this.snackBar.open(this.msg, null, { duration: 3000 });
+        });
+  }
 
-    getClientList() {
-      this.http.get<{message: string, clients: any}>('http://localhost:3000/api/clients/getClients')
+  getClientList() {
+    this.http.get<{ message: string, clients: any }>('http://localhost:3000/api/clients/getClients')
       .pipe(map((clientData) => {
         return clientData.clients.map(client => {
-          console.log(client);
-         return {
-            id : client._id,
+          //console.log(client);
+          return {
+            id: client._id,
             ClientCompanyName: client.companyName,
             ClientCompanyAddressInitial: client.companyAddressInitial,
-            ClientCompanyAddressPart2 : client.companyAddressPart2,
+            ClientCompanyAddressPart2: client.companyAddressPart2,
             ClientCompanyCity: client.companyCity,
             ClientCompanyPincode: client.companyPincode,
             ClientCompanyState: client.companyState,
             ClientCompanyCountry: client.companyCountry,
             ClientCompanyGSTN: client.companyGSTN,
             ClientCreator: client.clientCreator
-         };
+          };
         });
       }))
       .subscribe((TransformedClients) => {
-        console.log(TransformedClients);
-         this.clients = TransformedClients;
-         this.clientsUpdated.next([...this.clients]);
+        //console.log(TransformedClients);
+        this.clients = TransformedClients;
+        this.clientsUpdated.next([...this.clients]);
       });
-    }
+  }
 
-    getClientUpdateListener() {
-      return this.clientsUpdated.asObservable();
-    }
+  getClientUpdateListener() {
+    return this.clientsUpdated.asObservable();
+  }
 
+  updateClient(id: string, CompanyName: string, CompanyAddressInitial: string, CompanyAddressPart2: string,
+    CompanyCity: string, CompanyPincode: string, CompanyState: string, CompanyCountry: string,
+    CompanyGSTN: string) {
+    const client: Client = {
+      id: id,
+      ClientCompanyName: CompanyName,
+      ClientCompanyAddressInitial: CompanyAddressInitial,
+      ClientCompanyAddressPart2: CompanyAddressPart2,
+      ClientCompanyCity: CompanyCity,
+      ClientCompanyState: CompanyState,
+      ClientCompanyCountry: CompanyCountry,
+      ClientCompanyPincode: CompanyPincode,
+      ClientCompanyGSTN: CompanyGSTN
+    };
+    console.log(client);
+    this.http.put<{ message: string }>('http://localhost:3000/api/clients/updateClient/' + id, client)
+      .subscribe(response => {
+        this.snackBar.open(response.message, null, { duration: 3000 });
+        const updatedClients = [...this.clients];
+        const oldClientindex = updatedClients.findIndex(c => c.id === client.id);
+        updatedClients[oldClientindex] = client;
+        this.clients = updatedClients;
+        this.clientsUpdated.next([...this.clients]);
+      },
+        error => {
+          this.snackBar.open(error.message, null, { duration: 3000 });
+        });
+  }
 }
--- src/app/client-store/add-new-client/add-new-client.component.ts
+++ src/app/client-store/add-new-client/add-new-client.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormControl, Validators, NgForm, FormGroupDirective } from '@angular/forms';
 import { ClientManagementService } from '../ClientManagement.service';
 import { Client } from '../Client.module';
 import { Subscription } from 'rxjs';
@@ -21,10 +21,18 @@ export class AddNewClientComponent implements OnInit {
   companyCountry: string;
   companyPincode: string;
   companyGSTN: string;
+  options: Client[];
+  private clientSub: Subscription;
+  myControl: FormControl;
+  newOrEditFlag : boolean;
 
   constructor(private clientService: ClientManagementService) { }
 
+  @ViewChild(FormGroupDirective, {static:true}) formGroupDirective: FormGroupDirective;
+
   ngOnInit() {
+    this.newOrEditFlag = true;
+    this.myControl = new FormControl();
     this.form = new FormGroup({
       'CompanyName': new FormControl('Enter Company Name', { validators: [Validators.required] }),
       'CompanyAddressInitial': new FormControl('Enter Company Address 1', { validators: [Validators.required] }),
@@ -35,18 +43,62 @@ export class AddNewClientComponent implements OnInit {
       'CompanyPincode': new FormControl('Enter Company PinCode'),
       'CompanyGSTN': new FormControl('Enter Company GSTN')
     });
+    this.clientService.getClientList();
+    this.clientSub = this.clientService.getClientUpdateListener().subscribe((clients: Client[]) => {
+      this.options = clients;
+      console.log(this.options);
+    });
   }
 
 
 
-  addNewClient() {
-    this.clientService.AddNewClient(this.form.value.CompanyName,this.form.value.CompanyAddressInitial,
+  addOrUpdateClient() {
+    if(this.newOrEditFlag == true) {
+      this.clientService.AddNewClient(this.form.value.CompanyName,this.form.value.CompanyAddressInitial,
       this.form.value.CompanyAddressPart2,this.form.value.CompanyCity,this.form.value.CompanyState,
       this.form.value.CompanyCountry,this.form.value.CompanyPincode,this.form.value.CompanyGSTN
-       );
+      );
+    } else {
+      const client: Client = this.options.find(v => v.ClientCompanyName === this.form.get('CompanyName').value);
+      const companyName = this.form.controls['CompanyName'].value;
+      if(client) {
+        //console.log(client);
+        this.clientService.updateClient(client.id, companyName,this.form.value.CompanyAddressInitial,
+          this.form.value.CompanyAddressPart2,this.form.value.CompanyCity,this.form.value.CompanyPincode,
+          this.form.value.CompanyState,this.form.value.CompanyCountry,this.form.value.CompanyGSTN);
+        this.clearForm();
+      }
+    }
+
   }
 
   ViewMyClients() {
     this.clientService.getClientList();
   }
+
+  getClientFields(clientname) {
+    const client: Client = this.options.find(v => v.ClientCompanyName === clientname);
+    //console.log(client);
+    if (client) {
+      this.newOrEditFlag = false;
+      this.form.get('CompanyName').setValue(client.ClientCompanyName);
+      this.form.get('CompanyName').disable();
+      this.form.get('CompanyAddressInitial').setValue(client.ClientCompanyAddressInitial);
+      this.form.get('CompanyAddressPart2').setValue(client.ClientCompanyAddressPart2);
+      this.form.get('CompanyCity').setValue(client.ClientCompanyCity);
+      this.form.get('CompanyState').setValue(client.ClientCompanyState);
+      this.form.get('CompanyCountry').setValue(client.ClientCompanyCountry);
+      this.form.get('CompanyPincode').setValue(client.ClientCompanyPincode);
+      this.form.get('CompanyGSTN').setValue(client.ClientCompanyGSTN);
+    }
+  }
+
+  clearForm() {
+    this.newOrEditFlag = true;
+    this.myControl.setValue('');
+    this.form.get('CompanyName').enable();
+    this.form.reset();
+    this.formGroupDirective.resetForm();
+  }
+
 }
